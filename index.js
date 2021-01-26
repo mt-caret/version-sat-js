b7a7389b96d56c0fad39802434553a0d45cb27d2
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2021-01-26 15:08:32 +0000

    split into subcommands and add resolve functionality

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import fs from 'fs';
 import util from 'util';
 import npmFetch from 'npm-registry-fetch';
 import deepEqual from 'deep-equal';
-
-if (process.argv.length !== 4) {
-  console.error('usage: node index.js [path to package.json] [output file]');
-  process.exit(1);
-}
+import semver from 'semver';
 
 async function fetchVersions(packageName) {
   const result = await npmFetch.json(`/${packageName}`)
@@ -39,8 +35,8 @@ async function addDepsForPackage(packages, newPackageName) {
   }
 }
 
-(async () => {
-  const content = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+async function genClosure() {
+  const content = JSON.parse(fs.readFileSync(process.argv[3], 'utf8'));
 
   let packages = {};
 
@@ -49,6 +45,51 @@ async function addDepsForPackage(packages, newPackageName) {
   }
 
   console.log('collected dependencies, writing to file');
-  fs.writeFileSync(process.argv[3], JSON.stringify(packages));
+  fs.writeFileSync(process.argv[4], JSON.stringify(packages));
   console.log('done');
-})()
+}
+
+function resolve() {
+  const packages = JSON.parse(fs.readFileSync(process.argv[3], 'utf8'));
+  const packageName = process.argv[4];
+  const versionSpec = process.argv[5];
+
+  if (packages[packageName] == null) {
+    console.error(`package '${packageName}' not found`);
+    process.exit(1);
+  }
+
+  for (const { version } of packages[packageName]) {
+    if (semver.satisfies(version, versionSpec)) {
+      console.log(version);
+    }
+  }
+}
+
+if (process.argv.length < 3) {
+  console.error('supply subcommnd');
+  process.exit(1);
+}
+
+const subcommand = process.argv[2];
+switch (subcommand) {
+  case 'gen-closure':
+    if (process.argv.length !== 5) {
+      console.error('usage: node index.js gen-closure [path to package.json] [output file]');
+      process.exit(1);
+    }
+    getClosure();
+    break;
+  case 'resolve':
+    if (process.argv.length !== 6) {
+      console.error('usage: node index.js [path to dep closure] [package name] [version spec]');
+      process.exit(1);
+    }
+    resolve();
+    break;
+  default:
+    console.error(`unrecognized subcommand: ${subcommand}`);
+    process.exit(1);
+}
+
+
